c93dbbe843fc1983ccb2b7189738dbe950edde75
Arnav Jain <jainarnav107@gmail.com>
2025-09-26 15:05:14 +0530

    Update engine.hpp

--- src/engine.hpp
+++ src/engine.hpp
@@ -1,44 +1,22 @@
 #pragma once
-#include <unordered_map>
 #include <string>
-#include <mutex>
 #include <optional>
-#include <cstdint>
-#include <queue>
 #include <functional>
 
-struct ValueEntry {
-    std::string value;
-    std::int64_t expires_at = 0; 
-};
-
-struct ExpireItem {
-    std::int64_t expires_at;
-    std::string key;
-    bool operator>(ExpireItem const& o) const {
-        return expires_at > o.expires_at;
-    }
-};
-
-class Engine {
+class EngineSimple {
 public:
-    explicit Engine(std::function<void(const std::string&)> expiry_callback = nullptr);
-    ~Engine() = default;
 
-    void set(const std::string& key, const std::string& val, int ttl_seconds = 0);
+    explicit EngineSimple(std::function<void(const std::string&)> expiry_callback = nullptr);
+
+    void set(const std::string& key, const std::string& value, int ttl_seconds = 0);
     std::optional<std::string> get(const std::string& key);
     bool del(const std::string& key);
     bool exists(const std::string& key);
-    void sweep_expired(); 
-    long ttl(const std::string& key);
+    void sweep_expired();            
+    long ttl(const std::string& key); 
+    ~EngineSimple();
 
 private:
-    std::unordered_map<std::string, ValueEntry> store_;
-    std::mutex mu_;
-
-    std::priority_queue<ExpireItem, std::vector<ExpireItem>, std::greater<ExpireItem>> expiry_pq_;
-
-    std::function<void(const std::string&)> expiry_callback_;
-
-    static std::int64_t now_seconds();
+    struct Impl;
+    Impl* impl_;
 };
